464d722623c0429f4a6ddb898416a8812772aed6
aufall02 <aufaluzumaki@gmail.com>
2026-03-10 11:43:58 +0700

    setup site map and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+// https://astro.build/docs/configuration
 export default defineConfig({
+  site: "https://blog.ngecode.id",
+  integrations: [sitemap()],
   experimental: {
     fonts: [
       {
